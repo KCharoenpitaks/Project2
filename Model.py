e8ec681d50a9d30a8c6643f50d1682d4f54e1d59
KCharoenpitaks <50170318+KCharoenpitaks@users.noreply.github.com>
2020-02-19 01:25:05 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -8,25 +8,41 @@ import matplotlib.pyplot as plt
 from utils import *
 import pandas as pd
 
+class Classifier(nn.Module):
+    def __init__(self,inputs, hidden_size, n_layers, o_size):
+        super(Classifier, self).__init__()
+        self.inputs = inputs
+        self.hidden_size = hidden_size
+        self.fc1 = nn.Linear(inputs, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 1)
 
+    def forward(self, x):
+        x_1 = F.relu(self.fc1(x))
+        x_2 = F.relu(self.fc2(x_1))
+        x = F.sigmoid(self.fc3(x_2))
+        return x
 
-
+    
+ 
 class RNN(nn.Module):
-    def __init__(self, i_size, h_size, n_layers, o_size):
+    def __init__(self, input_size, hidden_size, n_layers,output_size):
         super(RNN, self).__init__()
 
-        self.rnn = nn.LSTM(
-            input_size=i_size,
-            hidden_size=h_size,
-            num_layers=n_layers
-        )
-        self.out = nn.Linear(h_size, o_size)
+        self.hidden_size = torch.zeros(1,hidden_size)
 
-    def forward(self, x, h_state):
-        r_out, hidden_state = self.rnn(x, h_state)
-        
-        hidden_size = hidden_state[-1].size(-1)
-        r_out = r_out.view(-1, hidden_size)
-        outs = self.out(r_out)
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
-        return outs, hidden_state
+    def forward(self, X):
+        #print(X.shape)
+        #print(hidden.shape)
+        combined = torch.cat((X, self.hidden_size), 1)
+        self.hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = F.sigmoid(output)#self.softmax(output)
+        return output, self.hidden
+
+    def initHidden(self):
+        return torch.zeros(len(X_train),HIDDEN_SIZE)
